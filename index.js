117bee42cf304709e1d8a5ab55ef39c9b439d2bb
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-25 15:36:37 -0300

    importa express para a aplicaçao

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
+const express = require('express');
+// const bodyParse = require('body-parser');
+// const router = require('./routers/router');
+
+const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
