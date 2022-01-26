c0197e056f3894c245f8ec03482b289ae43fcea9
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-26 14:04:02 -0300

    chama a funcao getColumn no arquivo de rotas

--- routers/router.js
+++ routers/router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
 .route('/products')
-.post(controllerProducts.createProducts);
+.post(controllerProducts.createProducts)
+.get(controllerProducts.getColumn);
 
 module.exports = router;
