c433a2b0eff2980d9cc0a259eb0e7bf78876112d
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-26 14:02:33 -0300

    implementa funcao getColumn na camada de controller

--- controllers/controllerProducts.js
+++ controllers/controllerProducts.js
@@ -2,10 +2,16 @@ const servicesProducts = require('../services/servicesProducts');
 
 const createProducts = async (req, res) => {
   const { name, quantity } = req.body;
-  servicesProducts.createProduct(name, quantity);
-  res.status(201).json({ message: 'deu certo' });
+  await servicesProducts.createProduct(name, quantity);
+  return res.status(201).json({ message: 'deu certo' });
+};
+
+const getColumn = async (req, res) => {
+  const result = await servicesProducts.getColumn('name');
+  return res.status(201).json(result);
 };
 
 module.exports = {
-  createProducts;
-}
+  createProducts,
+  getColumn,
+};
