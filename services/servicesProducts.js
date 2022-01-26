a313417f0a9725a342045168b304489742c0386e
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-26 16:32:53 -0300

    retira valor default do paramentro da getColumn

--- services/servicesProducts.js
+++ services/servicesProducts.js
@@ -2,10 +2,9 @@ const modelProducts = require('../models/modelProducts');
 
 const createProduct = async (name, quantity) => {
  await modelProducts.createProduct(name, quantity);
- return true;
 };
 
-const getColumn = async (param = '*') => {
+const getColumn = async (param) => {
 const result = await modelProducts.getColumn(param);
 return result;
 };
@@ -14,10 +13,3 @@ module.exports = {
   createProduct,
   getColumn,
 };
-// if(!name) return 400
-
-// if(name.length < 5 || typeof quantity !== Number || quantity < 0) return 422
-
-// if (getAllName().includes(name)) return 409
-
-// return 201
