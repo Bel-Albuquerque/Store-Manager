f8a67abd6ac70341e5da8f57f3858e2c6bb25c21
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-26 09:37:27 -0300

    Implementa createProducts no arquivo router.js

--- routers/router.js
+++ routers/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const controllerProducts = require('../controllers/controllerProducts');
 
 const router = express.Router();
 
 router
 .route('/products')
-.post();
+.post(controllerProducts.createProducts);
 
 module.exports = router;
