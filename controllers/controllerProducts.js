2eab475f6343215e7a2adaa17f7edadd5a1ec4f8
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-27 09:17:14 -0300

    implementa logica de getById usando getColumn

--- controllers/controllerProducts.js
+++ controllers/controllerProducts.js
@@ -1,6 +1,6 @@
 const servicesProducts = require('../services/servicesProducts');
 
-const findProduct = async (porductName) => {
+const findProductByName = async (porductName) => {
   const allProductsNames = await servicesProducts.getColumn('*');
   const result = allProductsNames.find((product) => product.name === porductName);
   return result;
@@ -9,16 +9,32 @@ const findProduct = async (porductName) => {
 const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
   await servicesProducts.createProduct(name, quantity);
-  const product = await findProduct(name);
+  const product = await findProductByName(name);
   return res.status(201).json(product);
 };
 
 const getColumn = async (req, res) => {
   const result = await servicesProducts.getColumn('*');
-  return res.status(201).json(result);
+  return res.status(200).json(result);
+};
+
+const findProductById = async (productId) => {
+  const allProductsNames = await servicesProducts.getColumn('*');
+  const result = allProductsNames.find((product) => product.id === productId);
+  return result;
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const product = await findProductById(Number(id));
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' }); 
+}
+  return res.status(200).json(product);
 };
 
 module.exports = {
   createProduct,
   getColumn,
+  getById,
 };
