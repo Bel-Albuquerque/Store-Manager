9079d94a211c0e9e9fdfcb670ad260ac3f5ef1ed
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-28 14:08:49 -0300

    cria createSalesProducts na camada servicesSales

---
+++ services/serviceSales.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable semi */
+const modelSales = require('../models/modelSales');
+const modelSalesProducts = require('../models/modelSalesProducts');
+
+const createSalesProducts = async (arraySale) => {
+  const id = await modelSales.createSales();
+  if (!arraySale) return false
+  const objSale = {
+    id,
+    itemsSold: arraySale,
+  };
+  
+  const array = []
+  arraySale.forEach(({ product_id, quantity }) => {
+    array.push(id, product_id, quantity);
+  });
+
+  await modelSalesProducts.createSalesProducts(array);
+
+  return objSale;
+};
+
+module.exports = {
+  createSalesProducts,
+};
