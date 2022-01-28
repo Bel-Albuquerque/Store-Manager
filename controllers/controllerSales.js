e73a6638cc6653ccf2cf359f77b57e9f8498b551
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-28 14:53:30 -0300

    implementa funcao getSalesProducts na camada controller

--- controllers/controllerSales.js
+++ controllers/controllerSales.js
@@ -7,6 +7,12 @@ if (!response) return res.status(404).json({ message: 'Sale not found' });
  return res.status(201).json(response);
 };
 
+const getSalesProducts = async (req, res) => {
+const allSales = await serviceSales.getSalesProducts();
+  return res.status(200).json(allSales);
+};
+
 module.exports = {
   createSales,
+  getSalesProducts,
 };
