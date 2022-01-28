2cfe915fdb39b44f5f3bb68ce6544964f4a8d519
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-28 15:21:43 -0300

    corrige no nome da funcao para getAllSalesAndProducts

--- controllers/controllerSales.js
+++ controllers/controllerSales.js
@@ -7,12 +7,13 @@ if (!response) return res.status(404).json({ message: 'Sale not found' });
  return res.status(201).json(response);
 };
 
-const getSalesProducts = async (req, res) => {
+const getAllSalesAndProducts = async (req, res) => {
 const allSales = await serviceSales.getSalesProducts();
   return res.status(200).json(allSales);
 };
 
 module.exports = {
   createSales,
-  getSalesProducts,
+  getAllSalesAndProducts,
+  getSalesAndProductsById,
 };
