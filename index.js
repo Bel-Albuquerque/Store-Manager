c7f4b1278ce1c6d3f1a4cf9aaa101b8c81b2eac7
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-28 14:13:42 -0300

    importa rota routerSales no index

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyparser = require('body-parser');
 const routerProducts = require('./routers/routerProducts');
+const routerSales = require('./routers/routerSales');
 
 const app = express();
 
 app.use(bodyparser.json());
 app.use('/products', routerProducts);
+app.use('/sales', routerSales);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
