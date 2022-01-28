4661a2784192fe372012b10ac5af0b44ac9d065c
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-28 17:15:45 -0300

    cria as rotas GET /sales e /sales:id

--- routers/routerSales.js
+++ routers/routerSales.js
@@ -5,12 +5,17 @@ const validations = require('../middlewares/validation');
 const router = express.Router();
 
 router
+.route('/:id')
+.get(controllerSales.getSalesAndProductsById);
+
+router
 .route('/')
 .post(
   validations.validationPoductIdTrue,
   validations.validationQuantityTrue,
   validations.validationQuantityIsInteger,
   controllerSales.createSales,
-);
+)
+.get(controllerSales.getAllSalesAndProducts);
 
 module.exports = router;
